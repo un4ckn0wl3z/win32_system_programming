dfc6a8da6338d3338faae42837e5ec3e2b8b3cdd
un4ckn0wl3z <slickerth@gmail.com>
2022-04-20 20:37:10 +0700

    do exercise for chapter 1

--- 009.foundation_exercises/009.foundation_exercises.cpp
+++ 009.foundation_exercises/009.foundation_exercises.cpp
@@ -2,6 +2,37 @@
 #include <stdio.h>
 int main()
 {
+	SYSTEM_INFO si;
+	::GetNativeSystemInfo(&si);
+
+	printf("Number of Logical Processors: %d\n", si.dwNumberOfProcessors);
+	printf("Page size: %d Bytes\n", si.dwPageSize);
+	printf("Processor Mask: 0x%p\n", (PVOID)si.dwActiveProcessorMask);
+	printf("Minimum process address: 0x%p\n", si.lpMinimumApplicationAddress);
+	printf("Maximum process address: 0x%p\n", si.lpMaximumApplicationAddress);
+
+	WCHAR buffer[256] = L"";
+	DWORD size = sizeof(buffer);
+	if (::GetComputerName(buffer, &size))
+	{
+		printf("ComputerName: %ws\n", buffer);
+	}
+
+	WCHAR dir[256] = L"";
+	int size_dir = sizeof(dir);
+	if (::GetWindowsDirectory(dir, size_dir))
+	{
+		printf("Windows Path: %ws\n", dir);
+	}
+
+	LARGE_INTEGER ticks;
+	if (::QueryPerformanceCounter(&ticks))
+	{
+		printf("QueryPerformanceCounter QuadPart: %d\n", ticks.QuadPart);
+		printf("QueryPerformanceCounter HighPart: %d\n", ticks.HighPart);
+		printf("QueryPerformanceCounter LowPart: %d\n", ticks.LowPart);
+
+	}
 
 	return 0;
 }
